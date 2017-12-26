8264e9bdad5d94f4bf7006b30fdcdce7b9caef30
zeroone001 <ilylys@163.com>
2017-12-26 11:29:22 +0800

    update this

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 #!/usr/bin/env node
-var args = require('minimist')(process.argv.slice(2));
+var arg = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+var exec = require('child_process').exec;
+var program = require('commander');
+const chalk = require('chalk');
+const log = console.log;
+//console.log(process.argv);
 
-//console.log(args);
+//console.log(arg);
 
-if(args.v || args.version ){
+/*if(args.v || args.version ){
 console.log('v1.0.0');
+}else if(args.help){
+  fs.createReadStream('./us.txt').pipe(process.stdout);  
+}*/
+function range(val){
+  return val.split('..').map(Number);
 }
+function list(val){
+  return val.split(',');
+}
+program.version(require('./package.json').version)
+  .usage('<path> [options]')
+  .option('-v --verbose', 'vvvvvvv')
+  .option('-o --output <path>', 'lujing')
+  .option('-l --list [item]', 'liebiao',list)
+  .option('-r --range <a>..<b>', 'a range', range)
+  .command('install [name]', 'install this name')
+  .action(function(env, options){
+    log('env_name', env);
 
+  })
+  .parse(process.argv);
 
+log(chalk.blue(program.args));
+//log('output',chalk.green.bgBlue(program.output));
+log(' range: %j, %j', program.range[0], program.range[1])
+log('list', program.list);
 
